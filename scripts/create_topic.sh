9431c74168bace49bf205b211a68b49a97284b3e
Roland Schlaefli <rolandschlaefli@gmail.com>
2019-03-25 09:10:48 +0100

    Add all topics to initialization script

--- scripts/create_topic.sh
+++ scripts/create_topic.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker-compose exec kafka kafka-topics --create --topic post --partitions 3 --replication-factor 1 --zookeeper 127.0.0.1:2181
+docker-compose exec kafka kafka-topics --create --topic post --partitions 1 --replication-factor 1 --zookeeper 127.0.0.1:2181
+docker-compose exec kafka kafka-topics --create --topic like --partitions 1 --replication-factor 1 --zookeeper 127.0.0.1:2181
+docker-compose exec kafka kafka-topics --create --topic comment --partitions 1 --replication-factor 1 --zookeeper 127.0.0.1:2181
