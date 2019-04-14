a58ee36a21ce8262859762f429a9a2c5d5fb643e
nicolas-kuechler <nico.kuechler@gmail.com>
2019-04-14 19:19:04 +0200

    implement top k aggregation function

--- social-network-analysis/src/main/java/ch/ethz/infk/dspa/recommendations/ops/TopKAggregateFunction.java
+++ social-network-analysis/src/main/java/ch/ethz/infk/dspa/recommendations/ops/TopKAggregateFunction.java
@@ -1,44 +1,85 @@
 package ch.ethz.infk.dspa.recommendations.ops;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.stream.Collectors;
 
 import org.apache.flink.api.common.functions.AggregateFunction;
 
 import ch.ethz.infk.dspa.recommendations.dto.PersonSimilarity;
 
 public class TopKAggregateFunction
-		implements AggregateFunction<PersonSimilarity, List<PersonSimilarity>, List<PersonSimilarity>> {
+		implements AggregateFunction<PersonSimilarity, PriorityQueue<PersonSimilarity>, List<PersonSimilarity>> {
 
 	private static final long serialVersionUID = 1L;
 
-	private final int k;
+	private final int k; // TODO [nku] need to checkpoint?
 
 	public TopKAggregateFunction(int k) {
 		this.k = k;
 	}
 
 	@Override
-	public List<PersonSimilarity> createAccumulator() {
-		// TODO Auto-generated method stub
-		return null;
+	public PriorityQueue<PersonSimilarity> createAccumulator() {
+
+		Comparator<PersonSimilarity> comp = new Comparator<PersonSimilarity>() {
+			@Override
+			public int compare(PersonSimilarity o1, PersonSimilarity o2) {
+				return Double.compare(o1.similarity(), o2.similarity());
+			}
+		};
+
+		return new PriorityQueue<PersonSimilarity>(this.k, comp);
 	}
 
 	@Override
-	public List<PersonSimilarity> add(PersonSimilarity value, List<PersonSimilarity> accumulator) {
-		// TODO Auto-generated method stub
-		return null;
+	public PriorityQueue<PersonSimilarity> add(PersonSimilarity value, PriorityQueue<PersonSimilarity> accumulator) {
+
+		if (accumulator.size() < this.k) {
+			// less that k elements => add
+			accumulator.add(value);
+		} else {
+			PersonSimilarity head = accumulator.peek();
+			if (head.similarity() < value.similarity()) {
+				accumulator.poll(); // remove head
+				accumulator.add(value); // add new larger
+			}
+		}
+		return accumulator;
 	}
 
 	@Override
-	public List<PersonSimilarity> getResult(List<PersonSimilarity> accumulator) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<PersonSimilarity> getResult(PriorityQueue<PersonSimilarity> accumulator) {
+		List<PersonSimilarity> list = accumulator.stream().collect(Collectors.toList());
+
+		Comparator<PersonSimilarity> cmp = new Comparator<PersonSimilarity>() {
+			@Override
+			public int compare(PersonSimilarity o1, PersonSimilarity o2) {
+				return Double.compare(o1.similarity(), o2.similarity());
+			}
+		};
+
+		// sort in descending order by similarity
+		Collections.sort(list, Collections.reverseOrder(cmp));
+
+		return list;
 	}
 
 	@Override
-	public List<PersonSimilarity> merge(List<PersonSimilarity> a, List<PersonSimilarity> b) {
-		// TODO Auto-generated method stub
-		return null;
+	public PriorityQueue<PersonSimilarity> merge(PriorityQueue<PersonSimilarity> q1,
+			PriorityQueue<PersonSimilarity> q2) {
+
+		q1.addAll(q2);
+		int size = q1.size();
+
+		// remove smallest size - k elements
+		for (int i = 0; i < size - this.k; i++) {
+			q1.remove();
+		}
+
+		return q1;
 	}
 
 }
